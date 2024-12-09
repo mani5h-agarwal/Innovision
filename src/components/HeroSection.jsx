679e2fd7976a6a73b5bf17082a6e022643921e84
Manish <manishagarwal428728@gmail.com>
2024-12-09 22:48:27 +0530

    changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,35 +1,39 @@
 import "./HeroSection.css";
 import { useState } from "react";
 
-
 const HeroSection = () => {
+  const [gradient, setGradient] = useState(
+    "linear-gradient(180deg, #030C17, #0A223F)"
+  ); // Initial gradient
 
-    const [gradient, setGradient] = useState('linear-gradient(180deg, #030C17, #0A223F)'); // Initial gradient
+  const [color, setColor] = useState("#081120"); // Initial color
 
-    const [color, setColor] = useState('#081120'); // Initial color
+  const handleLogoClick = () => {
+    // Toggle the gradient
+    setGradient(
+      (prevGradient) =>
+        prevGradient === "linear-gradient(180deg, #030C17, #0A223F)"
+          ? "linear-gradient(180deg, #030C17, #103562)"
+          : "linear-gradient(180deg, #030C17, #0A223F)" // Toggle back to the initial gradient
+    );
 
-    const handleLogoClick = () => {
-      // Toggle the gradient
-      setGradient(prevGradient => 
-        prevGradient === 'linear-gradient(180deg, #030C17, #0A223F)' 
-          ? 'linear-gradient(180deg, #030C17, #103562)' 
-          : 'linear-gradient(180deg, #030C17, #0A223F)' // Toggle back to the initial gradient
-      );
-
-      setColor(prevColor =>
-        prevColor === '#081120'
-          ? '#0A1628'
-          : '#081120' // Toggle back to the initial
-        );
-    };
+    setColor(
+      (prevColor) => (prevColor === "#081120" ? "#0A1628" : "#081120") // Toggle back to the initial
+    );
+  };
 
   return (
     <div className="outer" style={{ background: gradient }}>
-        <div className="circle0"></div>
-        <div className="circle01"></div>
-        <div className="circle02"></div>
+      <div className="circle0"></div>
+      <div className="circle01"></div>
+      <div className="circle02"></div>
       <div className="circle1">
-      <img src="innovision.svg" alt="inno" className="logo" onClick={handleLogoClick}  />
+        <img
+          src="innovision.svg"
+          alt="inno"
+          className="logo"
+          onClick={handleLogoClick}
+        />
 
         <div className="sanga-box">
           <img src="sanganitra.svg" alt="sanga" className="sanga" />
@@ -60,14 +64,12 @@ const HeroSection = () => {
           <a className="text">Yantrika</a>
         </div>
 
-            <div className="circle2-alias"></div>
+        <div className="circle2-alias"></div>
         <div className="circle2">
           <div className="circle3">
-          <div className="semi-circle" style={{background: color}}></div>
-
+            <div className="semi-circle" style={{ background: color }}></div>
           </div>
           <div className="circle4"></div>
-          
 
           <div className="title-box">
             <a className="subtitle">ACM NITK</a>
@@ -76,7 +78,6 @@ const HeroSection = () => {
         </div>
       </div>
     </div>
-
   );
 };
 
